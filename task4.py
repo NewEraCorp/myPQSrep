1cd5d19595f0db22c63daef71d7048feea4f58b6
unknown <mihaily12314@gmail.com>
2020-04-14 16:47:47 +0500

    задание 4

---
+++ task4.py
@@ -0,0 +1,9 @@
+import numpy as np
+#задание 4.1
+print('1:', sum(range(0,100,1)))
+
+#задание 4.2
+print('2: ', sum(range(0,int(input('2. введите верхний предел последовательности: ')),1)))
+
+#задание 4.3
+print('3:', np.mean(np.random.randint(100)))
